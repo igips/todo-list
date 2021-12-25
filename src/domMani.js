b223e26675a6e51b2fd7d4e651a8f0cd0e979ae0
igips <igor.szepietowski@gmail.com>
2021-12-25 18:09:20 +0100

    Fix adding task to project

--- src/domMani.js
+++ src/domMani.js
@@ -300,12 +300,16 @@ function createTaskEditor() {
     inputDate.setAttribute("required", true);
     secondContainer.appendChild(inputDate);
 
-    const inputProjectName = document.createElement("input");
-    inputProjectName.setAttribute("placeholder", "Project Name");
-    inputProjectName.setAttribute("type", "text");
-    inputProjectName.setAttribute("id", "projectNameValue");
-    inputProjectName.setAttribute("class", "set-project-name");
-    secondContainer.appendChild(inputProjectName);
+
+    if(document.getElementById("list-name").textContent == "Home") {
+        const inputProjectName = document.createElement("input");
+        inputProjectName.setAttribute("placeholder", "Project Name");
+        inputProjectName.setAttribute("type", "text");
+        inputProjectName.setAttribute("id", "projectNameValue");
+        inputProjectName.setAttribute("class", "set-project-name");
+        secondContainer.appendChild(inputProjectName);
+    }
+    
 
     const imgAccept = document.createElement("button");
     imgAccept.setAttribute("id", "acceptButton");
@@ -383,14 +387,23 @@ function clearForm() {
     document.getElementById("titleOfTask").value = "";
     document.getElementById("detailsOfTask").value = "";
     document.getElementById("datePicker").value = "";
-    document.getElementById("projectNameValue").value = "";
+    if(document.getElementById("projectNameValue") != null) {
+        document.getElementById("projectNameValue").value = "";
+    }
+    
 }
 
 function getDataFromTaskFormAndCreateTask() {
     const title = document.getElementById("titleOfTask").value;
     const details = document.getElementById("detailsOfTask").value;
     const date = document.getElementById("datePicker").value;
-    const project = document.getElementById("projectNameValue").value;
+    let project = "";
+    if(document.getElementById("list-name").textContent == "Home") {
+        project = document.getElementById("projectNameValue").value;
+    } else if (document.getElementById("list-name").textContent != "Home") {
+        project = document.getElementById("list-name").textContent.replace(/\s+/g, '');
+    }
+    
     
 
     const task = Task(title, details, date, project, false, setIdForTask());
